3e9fd73c4067f7d20b7251af479c53f35bd0337d
leosantosw <leonardosantosx0077@gmail.com>
2025-02-21 08:43:37 -0300

    feat: create router

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func HandleRequests() {
+	http.HandleFunc("/", Home)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the home page!")
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	HandleRequests()
 }
