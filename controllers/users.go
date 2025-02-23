4e450cadc91620ec8e195ea0434a929518b9eb97
leosantosw <leonardosantosx0077@gmail.com>
2025-02-23 14:52:07 -0300

    feat: get url param

--- controllers/users.go
+++ controllers/users.go
@@ -4,10 +4,18 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/leosantosw/go-api-rest/models"
 )
 
-func Users(w http.ResponseWriter, r *http.Request) {
+func ListUsers(w http.ResponseWriter, r *http.Request) {
 	users := models.ListUsers()
 	json.NewEncoder(w).Encode(users)
+}
+
+func FindUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	user := models.FindUser(id)
+	json.NewEncoder(w).Encode(user)
 }
